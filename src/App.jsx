a1e44e1480fe099ca81b683e50fb28353dd079f5
Sarkix <142878605+Sarkix@users.noreply.github.com>
2023-12-06 12:31:33 +0100

    Añadir archivos ejercicio 24 React.

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import FocusableInput from "./FocusableInput";
+
+function App() {
+    return (
+        <div>
+            <h1>My React App</h1>
+            {/* Agregamos el componente FocusableInput */}
+            <FocusableInput />
+            {/* Otros componentes o contenido de la aplicación */}
+        </div>
+    );
+}
+
+export default App;
